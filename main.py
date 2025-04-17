abbff385c62137fab03ae1819767e27618153824
Ted <theodorehmorris@gmail.com>
2025-04-16 17:02:41 -0700

    Initial Push

---
+++ main.py
@@ -0,0 +1,29 @@
+#variables and expressions
+#Data type for these variables are string
+
+firstName = "Ted"
+lastName = "Morris"
+
+print(firstName, lastName+".")
+
+#Data type for these variables are integer
+
+numbers = 2
+
+print(numbers * numbers)
+
+#Data type for these variables is float (decimal #s)
+
+number = 3.4
+
+print(number * number)
+
+#intro to input()
+
+#projects "please..." to user, stores user input from run tab to variable called name
+name = input("please enter a number?")
+
+#converts above (name = input("please enter a number?")) into an integer
+name = int(name)
+
+print(name + 5)
